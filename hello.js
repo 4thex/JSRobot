2214b36666f9efd4ad6c0978454c9b89c839d8f4
Palle Cogburn <palle@4thex.com>
2015-09-26 09:13:58 -0700

    Respond to key events

--- hello.js
+++ hello.js
@@ -1,10 +1,20 @@
 window.addEventListener("load", function() {
-
+  var speed = 100;
   var canvas = document.querySelector("canvas");
   canvas.addEventListener("click", function() {
     var image = document.querySelector("#snapshot");
     image.src = canvas.toDataURL();
   });
+
+  document.addEventListener("keydown", function(event) {
+    console.log(event.type + ":" + (event.key || event.which));
+    speed++;
+    robot.setSpeeds(speed/4, speed);
+  }, false);
+
+  document.addEventListener("keyup", function(event) {
+    console.log(event.type + ":" + (event.key || event.which));
+  }, false);
 
   var field = Field();
   var robot = JSRobot({x: 0, y: 0}, "red");
